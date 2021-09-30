93419863a3a4551f5b4b52ee23d0f8d8aab3aee8
AlanwMelo <amelo967@gmail.com>
2021-09-30 20:20:44 -0300

    Controller e animações configurados

--- lib/screens/open_map.dart
+++ lib/screens/open_map.dart
@@ -4,30 +4,72 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class OpenMap extends StatefulWidget {
-  final MapController mapController;
   final List<Marker> markerList;
 
-  const OpenMap(
-      {Key? key, required this.mapController, required this.markerList})
-      : super(key: key);
+  const OpenMap({Key? key, required this.markerList}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _NewMapTestState();
+  State<StatefulWidget> createState() => NewMapTestState();
 }
 
-class _NewMapTestState extends State<OpenMap> {
+class NewMapTestState extends State<OpenMap> with TickerProviderStateMixin {
+  //MapController mapController = MapController();
+  late final MapController mapController;
+
+  @override
+  void initState() {
+    super.initState();
+    mapController = MapController();
+  }
+
+  /// Animação retirada de https://github.com/fleaflet/flutter_map
+  void animatedMapMove(LatLng destLocation, double destZoom) {
+    // Create some tweens. These serve to split up the transition from one location to another.
+    // In our case, we want to split the transition be<tween> our current map center and the destination.
+    final _latTween = Tween<double>(
+        begin: mapController.center.latitude, end: destLocation.latitude);
+    final _lngTween = Tween<double>(
+        begin: mapController.center.longitude, end: destLocation.longitude);
+    final _zoomTween = Tween<double>(begin: mapController.zoom, end: destZoom);
+
+    // Create a animation controller that has a duration and a TickerProvider.
+    var controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    // The animation determines what path the animation will take. You can try different Curves values, although I found
+    // fastOutSlowIn to be my favorite.
+    Animation<double> animation =
+        CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
+
+    controller.addListener(() {
+      mapController.move(
+          LatLng(_latTween.evaluate(animation), _lngTween.evaluate(animation)),
+          _zoomTween.evaluate(animation));
+    });
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.dispose();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.dispose();
+      }
+    });
+
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FlutterMap(
+        mapController: mapController,
         options: MapOptions(
-          controller: widget.mapController,
           center: new LatLng(51.5, -0.09),
-
-          /// Alterar para mapbounds !!!!!!
+          // Alterar para mapbounds !!!!!!
           zoom: 13.0,
           debugMultiFingerGestureWinner: true,
           enableMultiFingerGestureRace: true,
+          allowPanning: false,
+          nePanBoundary:
         ),
         layers: [
           TileLayerOptions(
@@ -42,7 +84,14 @@ class _NewMapTestState extends State<OpenMap> {
                 height: 80.0,
                 point: LatLng(51.5, -0.09),
                 builder: (ctx) => Container(
-                  child: FlutterLogo(),
+                  child: GestureDetector(
+                      onDoubleTap: () async {
+                        print('tap');
+
+                        mapController.move(
+                            LatLng(-22.910454, -47.062978), 13.0);
+                      },
+                      child: FlutterLogo()),
                 ),
               ),
             ],
