499aefd6bfc6498092b0311c51ce206a4074991b
AlanwMelo <amelo967@gmail.com>
2021-10-01 14:59:17 -0300

    Definição do layout da lista

--- lib/screens/map_and_photos.dart
+++ lib/screens/map_and_photos.dart
@@ -17,6 +17,28 @@ class _MapAndPhotos extends State<MapAndPhotos> {
   double screenUseablewidth = 0;
   GlobalKey<NewMapTestState> openMapController = GlobalKey<NewMapTestState>();
   MapController openMapController22 = MapController();
+  List<int> testList = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+  ];
 
   @override
   void initState() {
@@ -64,18 +86,21 @@ class _MapAndPhotos extends State<MapAndPhotos> {
                         Container(
                           width: useAbleWidth,
                           height: (useAbleHeight - mapHeight) * 0.2,
-                          color: Colors.blue,
-                          child: ElevatedButton(
-                              onPressed: () {
-                                _moveMap();
-                              },
-                              child: Text('Lista')),
+                          color: Colors.blue.withOpacity(0.3),
+                          child: Row(
+                            children: [
+                              imgList((useAbleHeight - mapHeight) * 0.2),
+                              addMoreButton((useAbleHeight - mapHeight) * 0.2),
+                            ],
+                          ),
                         ),
                         Expanded(
                           child: Container(
                               width: useAbleWidth,
                               color: Colors.black,
-                              child: Center(child: Image.network("https://www.wallpapertip.com/wmimgs/165-1657683_primal-kyogre-wallpapers-wallpaper-cave-shiny-primal-kyogre.jpg"))),
+                              child: Center(
+                                  child: Image.network(
+                                      "https://www.wallpapertip.com/wmimgs/165-1657683_primal-kyogre-wallpapers-wallpaper-cave-shiny-primal-kyogre.jpg"))),
                         ),
                         /*Expanded(
                           child: Container(
@@ -101,5 +126,57 @@ class _MapAndPhotos extends State<MapAndPhotos> {
     NewMapTestState mapController = openMapController.currentState!;
 
     mapController.animatedMapMove(LatLng(-22.910454, -47.062978), 13.0);
+  }
+
+  imgList(double size) {
+    /// Usar o valor de height para garantir os quadrado em qualquer tamanho de tela
+    return Container(
+      width: MediaQuery.of(context).size.width - 45,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: testList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: EdgeInsets.all(2),
+            padding: EdgeInsets.all(2),
+            color: Colors.red,
+            width: size,
+            child: Column(
+              children: [
+                Container(
+                  height: size * 0.7,
+                  color: Colors.amber,
+                ),
+                Expanded(
+                  child: Container(
+                    width: size,
+                    color: Colors.white30,
+                    child: Center(child: Text('${testList[index]}')),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  addMoreButton(double height) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          left: BorderSide(color: Colors.black, width: 1)
+        )
+      ),
+      width: 45,
+      height: height,
+      child: TextButton(
+        onPressed: () {
+          _moveMap();
+        },
+        child: Icon(Icons.add_a_photo_outlined),
+      ),
+    );
   }
 }
