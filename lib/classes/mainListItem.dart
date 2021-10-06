fe8002a494ce6f10988f906c0d9651fcb5a599f4
AlanwMelo <amelo967@gmail.com>
2021-10-06 11:09:16 -0300

    Ajustes para melhor utilização da lista

--- lib/classes/mainListItem.dart
+++ lib/classes/mainListItem.dart
@@ -3,8 +3,10 @@ import 'package:photo_tracker/classes/listItem.dart';
 
 class MainListItem {
   final String name;
-  final List<ListItem> listOfListItems;
+  final ListItem firstItem;
+  final DateTime created;
+  final int itemCount;
 
-  MainListItem(this.name, this.listOfListItems);
+  MainListItem(this.name, this.firstItem, this.created, this.itemCount);
 
 }
